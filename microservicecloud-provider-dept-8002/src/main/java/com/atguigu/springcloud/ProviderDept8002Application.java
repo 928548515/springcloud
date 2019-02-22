231ee12c224f4e88c1747ff45b55bd91dea3f65d
928548515 <928548515@qq.com>
2019-02-22 11:21:44 +0800

    eureka service 7001

---
+++ microservicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/ProviderDept8002Application.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProviderDept8002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderDept8002Application.class, args);
+    }
+
+}
