7d190b83ad1c1e79aa829ace5601058d304d57e3
928548515 <928548515@qq.com>
2019-02-26 17:02:43 +0800

    eureka service 7001

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/MicroservicecloudProviderDeptHystrix8001Application.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker //对hystrixR熔断机制的支持
+public class   MicroservicecloudProviderDeptHystrix8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudProviderDeptHystrix8001Application.class, args);
+    }
+
+}
